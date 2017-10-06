e896c8f98219ccc6dedee5a1468c402d16372206
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-07 04:16:35 +0900

    Fix bug in grafana setup script

--- scripts/install-grafana-docker.sh
+++ scripts/install-grafana-docker.sh
@@ -4,6 +4,6 @@ set -euf -o pipefail
 
 # Run Grafana automatically with Docker
 docker run -d -p 3000:3000 \
-  --restart unless-stopped
+  --restart unless-stopped \
   -v /var/lib/grafana:/var/lib/grafana \
   grafana/grafana

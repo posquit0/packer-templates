455399e885b54d43906852d249633fce80ba14ce
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-08 14:50:17 +0900

    Add node.js packer template

---
+++ scripts/install-nodejs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+NODEJS_VERSION=8
+
+# Set up the latest Node.js repository and add official GPG key
+curl -sL "https://deb.nodesource.com/setup_$NODEJS_VERSION.x" | bash -
+
+# Set up the latest Yarn repository and add official GPG key
+add-apt-repository \
+  "deb https://dl.yarnpkg.com/debian/ stable main"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+
+# Install Node.js and Yarn
+apt-get update -qq && apt-get install -y nodejs yarn

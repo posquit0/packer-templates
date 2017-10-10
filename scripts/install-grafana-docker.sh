f336858422b928d8e4000c2e83c4567bc6b30a6a
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-10 22:04:29 +0900

    Use specific version for grafana

--- scripts/install-grafana-docker.sh
+++ scripts/install-grafana-docker.sh
@@ -2,8 +2,10 @@
 
 set -euf -o pipefail
 
+GRAFANA_VERSION=4.5.2
+
 # Run Grafana automatically with Docker
 docker run -d -p 3000:3000 \
   --restart unless-stopped \
   -v /var/lib/grafana:/var/lib/grafana \
-  grafana/grafana
+  grafana/grafana:$GRAFANA_VERSION

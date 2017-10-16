8a3ba573329963aa65d8e06f3d6a7ea96b0eb4dd
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-16 16:36:03 +0900

    Add grafana configuration file

--- scripts/install-grafana-docker.sh
+++ scripts/install-grafana-docker.sh
@@ -4,8 +4,13 @@ set -euf -o pipefail
 
 GRAFANA_VERSION=4.5.2
 
+# Copy configuration files
+cp /tmp/files/grafana/grafana.ini $PWD
+
 # Run Grafana automatically with Docker
 docker run -d -p 3000:3000 \
   --restart unless-stopped \
   -v /var/lib/grafana:/var/lib/grafana \
+  -v $PWD/grafana.conf:/etc/grafana/grafana.ini:ro \
+  --name=grafana \
   grafana/grafana:$GRAFANA_VERSION

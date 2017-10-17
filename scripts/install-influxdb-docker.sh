feff2df978ac4c198f07952a799d16a13ed718cd
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-17 17:30:33 +0900

    Update to restart automatically influxdb

--- scripts/install-influxdb-docker.sh
+++ scripts/install-influxdb-docker.sh
@@ -14,6 +14,7 @@ cp /tmp/files/influxdb/types.db $PWD
 
 # Run InfluxDB automatically with Docker
 docker run -d -p 8086:8086 -p 25826:25826/udp \
+  --restart unless-stopped \
   -v /var/lib/influxdb:/var/lib/influxdb \
   -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
   -v $PWD/types.db:/usr/share/collectd/types.db:ro \

4672a7ce5ac0ad86d727d9e684318d4f77b65d37
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-18 12:26:34 +0900

    Add plugins to grafana installation

--- scripts/install-grafana-docker.sh
+++ scripts/install-grafana-docker.sh
@@ -3,6 +3,7 @@
 set -euf -o pipefail
 
 GRAFANA_VERSION=4.5.2
+GRAFANA_PLUGINS="raintank-worldping-app"
 
 # Copy configuration files
 cp /tmp/files/grafana/grafana.ini $PWD
@@ -12,5 +13,6 @@ docker run -d -p 3000:3000 \
   --restart unless-stopped \
   -v /var/lib/grafana:/var/lib/grafana \
   -v $PWD/grafana.ini:/etc/grafana/grafana.ini \
+  -e "GF_INSTALL_PLUGINS=$GRAFANA_PLUGINS"\
   --name=grafana \
   grafana/grafana:$GRAFANA_VERSION
